4c27b6863173386ce49e39a944c2f9a68cd837ff
Aleksandra Grabska <gucwa.ola@gmail.com>
2019-09-10 10:27:46 +0200

    Add plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: 'src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})];
 
 module.exports = (env) => {
+  if (env=== 'production') {
+    plugins.push(
+      new OptimizeJsPlugin({
+        sourceMap: false
+      })
+    )
+  }
+
   const environment = env || 'production';
 
   return {
@@ -25,10 +41,9 @@ module.exports = (env) => {
     				options: {
     					modules: true
     				}
-    			}
-    		  ]
+          }]
     		}
-    	]
-	}
+    	]}
   }
+  plugins: plugins
 };
