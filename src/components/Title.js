2b170b40ebc2b505cde72ccf587118b1b7a60557
Aleksandra Grabska <gucwa.ola@gmail.com>
2019-09-10 09:46:47 +0200

    Update js code

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
-const Title = props => <h1>{props.title}</h1>
+const Title = props => <div>
+                        <h1>{props.title}</h1>
+    //                  <p>Tasks: ({})</p> Tutaj ma się wyświetlać liczba zadań
+                      </div>
 
-export default Title
+export default Title;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import uuid from 'uuid';
 import style from './App.css';
-import Title from '../components/Title.js'
+import Title from '../components/Title.js';
+import TodoList from '../components/TodoList.js';
 
 class App extends React.Component {
 	constructor(props) {
